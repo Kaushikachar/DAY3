0ba49bbe28ababd600a14f90f9be59878b79c7a7
Kaushikachar <147932881+Kaushikachar@users.noreply.github.com>
2023-10-22 23:20:40 +0530

    Add files via upload

---
+++ Body.jsx
@@ -0,0 +1,14 @@
+import React from "react"
+const Body =() =>{
+    return(
+    <>
+        <hr/>
+        <h3>About me</h3>
+        <p>
+        Subhas Chandra Bose was an Indian nationalist whose defiance of British authority in India made him a hero among many Indians, but his wartime alliances with Nazi Germany and Imperial Japan left a legacy vexed by authoritarianism, anti-Semitism, and military failure.
+        </p>
+
+    </>
+    );
+};
+export default Body;
---
+++ Header.jsx
@@ -0,0 +1,18 @@
+import React  from  'react'
+import './Header.css'
+
+function Header(){
+    return (
+        <div id='box'>
+            <h1>Subash Chandra Bose</h1>
+            <img id="imgg"src="https://imgeng.jagran.com/images/2021/jan/bose1611248119797.jpg" width={250}
+            alt="Loading......." />
+
+        </div>
+        
+        
+    )
+
+}
+
+export default Header
